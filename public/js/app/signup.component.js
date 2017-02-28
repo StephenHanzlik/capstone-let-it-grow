5c2f7d7291291dca685b89480f1494c37a22cd82
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-02-28 13:53:50 -0700

    built out sign up form better and have the controller almost set up to post an http to route.   now need to build post route

--- public/js/app/signup.component.js
+++ public/js/app/signup.component.js
@@ -3,49 +3,46 @@
 
   angular.module('app')
     .component('signUp', {
-      controller: function($state) {
+      controller: function($state, $http) {
         const vm = this;
         vm.signUp = signUp;
 
         function signUp() {
           console.log(vm.signupForm);
-          if (!vm.signupForm.email) {
-            console.log("nothing here");
-          } else if (!vm.signupForm.username) {
-            window.plugins.toast.showWithOptions({
-              message: "Please enter an username",
-              duration: "long",
-              position: "center",
-              addPixelsY: -40
-            });
-          } else if (!vm.signupForm.password) {
-            window.plugins.toast.showWithOptions({
-              message: "Please enter an password",
-              duration: "long",
-              position: "center",
-              addPixelsY: -40
-            });
+          if (!vm.signupForm.Email) {
+            console.log("Invalid Email");
+          } else if (!vm.signupForm.Username) {
+            console.log("Invalid Username");
+          } else if (!vm.signupForm.Password) {
+            console.log("Invalid Password");
           } else {
-            vm.password = vm.signupForm.password;
-            userService.makeNew(vm.signupForm.email, vm.signupForm.username, vm.signupForm.password)
-              .then(function success(userData) {
-                vm.data = userData;
-                vm.data.password = vm.password;
-                userService.login(vm.data.email, vm.data.password)
-                  .then((res) => {
-                    vm.data = res;
-                    $state.go('tabsController.profile');
-                  }, (error) => {
-                    $state.go('tabsController.login');
-                  });
-              }, function error(error) {
-                window.plugins.toast.showWithOptions({
-                  message: `Failed to create user: ${error.data}.\nPlease try again.`,
-                  duration: "long",
-                  position: "center",
-                  addPixelsY: -40
-                });
-              });
+
+            $http.post("http://localhost:3000/users/", vm.signupForm)
+              .then(function(response) {
+                $state.go('profile');
+              })
+
+
+            // vm.password = vm.signupForm.Password;
+            // userService.makeNew(vm.signupForm.email, vm.signupForm.username, vm.signupForm.password)
+            //   .then(function success(userData) {
+            //     vm.data = userData;
+            //     vm.data.password = vm.password;
+            //     userService.login(vm.data.email, vm.data.password)
+            //       .then((res) => {
+            //         vm.data = res;
+            //         $state.go('tabsController.profile');
+            //       }, (error) => {
+            //         $state.go('tabsController.login');
+            //       });
+            //   }, function error(error) {
+            //     window.plugins.toast.showWithOptions({
+            //       message: `Failed to create user: ${error.data}.\nPlease try again.`,
+            //       duration: "long",
+            //       position: "center",
+            //       addPixelsY: -40
+            //     });
+            //   });
           }
         }
         // vm.$onInit = function() {
