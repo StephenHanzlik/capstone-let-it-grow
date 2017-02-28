423ada69137f3be2b32ce28e2d6dd2a0fb50cf9b
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-02-28 12:47:37 -0700

    have signup form posting object to controller

--- public/js/app/signup.component.js
+++ public/js/app/signup.component.js
@@ -3,13 +3,18 @@
 
   angular.module('app')
     .component('signUp', {
-      controller: function($state, houseService) {
+      controller: function($state) {
         const vm = this
 
+        vm.signUp = signUp;
+
+        function signUp() {
+          console.log(vm.signupForm);
+        }
         // vm.$onInit = function() {
         //   vm.houses = houseService.houses
         // }
-        //
+
         // vm.addHouse = function() {
         //   houseService.addHouse(vm.house)
         //   $state.go('show-house', { id: vm.house.id })
