b4ba5195446e61769ac8fe712d58ac4e443995e1
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-02-28 13:41:55 -0700

    have sign up form user validation done, object logging from controller

--- public/js/app/login.component.js
+++ public/js/app/login.component.js
@@ -3,11 +3,56 @@
 
   angular.module('app')
     .component('login', {
-      controller: function(houseService) {
-        const vm = this
+      controller: function(tokenService) {
+        const vm = this;
+        vm.login = login;
+        vm.$onInit = onInit;
 
-        vm.$onInit = function() {
-          vm.houses = houseService.houses
+        function onInit() {
+          if (tokenService.checkToken()) {
+            console.log('You are already logged in!');
+            $state.go('tabsController.home');
+          } else {
+            userService.logout();
+            console.log('You are not logged in!');
+          }
+        }
+
+        function login() {
+
+          if (!vm.loginForm.email) {
+            window.plugins.toast.showWithOptions({
+              message: "Please enter an email",
+              duration: "long",
+              position: "center",
+              addPixelsY: -40
+            });
+          } else if (!vm.loginForm.password) {
+            window.plugins.toast.showWithOptions({
+              message: "Please enter an password",
+              duration: "long",
+              position: "center",
+              addPixelsY: -40
+            });
+          } else {
+            userService.login(vm.loginForm.email, vm.loginForm.password)
+              .then((res) => {
+                console.log('login successful:', res.data);
+                console.log('userService:', userService.userData);
+                vm.data = userService.userData;
+
+                $state.go('tabsController.home');
+              }, (error) => {
+                console.log('login failed:', error.data);
+                window.plugins.toast.showWithOptions({
+                  message: `Login failed: ${error.data}.\n Please try again.`,
+                  duration: "long",
+                  position: "center",
+                  addPixelsY: -40
+                });
+              });
+          }
+
         }
       },
       // template: `<h1>I am login template</h1>`
--- public/js/app/signup.component.js
+++ public/js/app/signup.component.js
@@ -4,12 +4,49 @@
   angular.module('app')
     .component('signUp', {
       controller: function($state) {
-        const vm = this
-
+        const vm = this;
         vm.signUp = signUp;
 
         function signUp() {
           console.log(vm.signupForm);
+          if (!vm.signupForm.email) {
+            console.log("nothing here");
+          } else if (!vm.signupForm.username) {
+            window.plugins.toast.showWithOptions({
+              message: "Please enter an username",
+              duration: "long",
+              position: "center",
+              addPixelsY: -40
+            });
+          } else if (!vm.signupForm.password) {
+            window.plugins.toast.showWithOptions({
+              message: "Please enter an password",
+              duration: "long",
+              position: "center",
+              addPixelsY: -40
+            });
+          } else {
+            vm.password = vm.signupForm.password;
+            userService.makeNew(vm.signupForm.email, vm.signupForm.username, vm.signupForm.password)
+              .then(function success(userData) {
+                vm.data = userData;
+                vm.data.password = vm.password;
+                userService.login(vm.data.email, vm.data.password)
+                  .then((res) => {
+                    vm.data = res;
+                    $state.go('tabsController.profile');
+                  }, (error) => {
+                    $state.go('tabsController.login');
+                  });
+              }, function error(error) {
+                window.plugins.toast.showWithOptions({
+                  message: `Failed to create user: ${error.data}.\nPlease try again.`,
+                  duration: "long",
+                  position: "center",
+                  addPixelsY: -40
+                });
+              });
+          }
         }
         // vm.$onInit = function() {
         //   vm.houses = houseService.houses
