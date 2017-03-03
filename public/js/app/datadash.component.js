12e0e2bf854dc38587a42593d8fc2a43a92cab03
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-03-02 19:51:55 -0700

    was able to integrate third dail for moisutre content

--- public/js/app/datadash.component.js
+++ public/js/app/datadash.component.js
@@ -35,6 +35,120 @@
             // createdAtArray.push(response.data[i].created_at);
           }
 
+          window.feed = function(callback) {
+    var tick = {};
+    tick.plot0 = Math.ceil(350 + (Math.random() * 500));
+    callback(JSON.stringify(tick));
+  };
+
+  var myConfig = {
+   	type: "gauge",
+   	globals: {
+   	  fontSize: 25
+   	},
+   	plotarea:{
+   	  marginTop:80
+   	},
+   	plot:{
+   	  size:'100%',
+   	  valueBox: {
+   	    placement: 'center',
+   	    text:'%v', //default
+   	    fontSize:35,
+   	    rules:[
+   	      {
+   	        rule: '%v >= 700',
+   	        text: '%v<br>EXCELLENT'
+   	      },
+   	      {
+   	        rule: '%v < 700 && %v > 640',
+   	        text: '%v<br>Good'
+   	      },
+   	      {
+   	        rule: '%v < 640 && %v > 580',
+   	        text: '%v<br>Fair'
+   	      },
+   	      {
+   	        rule: '%v <  580',
+   	        text: '%v<br>Bad'
+   	      }
+   	    ]
+   	  }
+   	},
+    tooltip:{
+      borderRadius:5
+    },
+   	scaleR:{
+  	  aperture:180,
+  	  minValue:300,
+  	  maxValue:850,
+  	  step:50,
+  	  center:{
+  	    visible:false
+  	  },
+  	  tick:{
+  	    visible:false
+  	  },
+  	  item:{
+  	    offsetR:0,
+  	    rules:[
+  	      {
+  	        rule:'%i == 9',
+  	        offsetX:15
+  	      }
+  	    ]
+  	  },
+  	  labels:['300','','','','','','580','640','700','750','','850'],
+  	  ring:{
+  	    size:50,
+  	    rules:[
+  	      {
+  	        rule:'%v <= 580',
+  	        backgroundColor:'#E53935'
+  	      },
+  	      {
+  	        rule:'%v > 580 && %v < 640',
+  	        backgroundColor:'#EF5350'
+  	      },
+  	      {
+  	        rule:'%v >= 640 && %v < 700',
+  	        backgroundColor:'#FFA726'
+  	      },
+  	      {
+  	        rule:'%v >= 700',
+  	        backgroundColor:'#29B6F6'
+  	      }
+  	    ]
+  	  }
+   	},
+    refresh:{
+        type:"feed",
+        transport:"js",
+        url:"feed()",
+        interval:1500,
+        resetTimeout:1000
+    },
+  	series : [
+  		{
+  			values : [755], // starting value
+  			backgroundColor:'black',
+  	    indicator:[10,10,10,10,0.75],
+  	    animation:{
+          effect:2,
+          method:1,
+          sequence:4,
+          speed: 900
+       },
+  		}
+  	]
+  };
+
+  zingchart.render({
+  	id : 'myChart2',
+  	data : myConfig,
+  	height: 500,
+  	width: '100%'
+  });
 
 
         $scope.myJson = {
