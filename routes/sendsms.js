cee1020ede1dda0d42d53b1b0c311210231c1c77
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-03-05 22:25:15 -0700

    Sending SMS on hitting post route for sendsms route

--- routes/sendsms.js
+++ routes/sendsms.js
@@ -1,18 +1,20 @@
 'use strict';
 
-var express = require('express');
-var route = express.Router();
+'use strict';
 
-var bcrypt = require('bcrypt');
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex');
+const boom = require('boom');
 
-var db = require('../knexfile.js')['development'];
-var knex = require('knex')(db);
 
-route.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
   res.send('Root');
 });
 
-route.post('/', (req, res, next) => {
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+  if(req.body.lightOff === "1"){
     var accountSid = 'AC674af2aaed607cbb23d6d2e718c30d6f';
     var authToken = 'cceebb0dbcbfd2f072e45f83eae2b2b5';
 
@@ -20,23 +22,25 @@ route.post('/', (req, res, next) => {
     var client = require('twilio')(accountSid, authToken);
 
     client.messages.create({
-        // to: "+16109844474",//Me
-        to: "+14848666955",//Keller
+      to: "+16109844474",//Me
+      // to: "+14848666955",//Keller
 
-        from: "+14846265179",
-        body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+      from: "+14846265179",
+      body: "This is the ship that made the Kessel Run in fourteen parsecs?",
     }, function(err, message) {
-        console.log(message.sid);
+      console.log(message.sid);
     });
-
-
-
-
+    // next(boom.create(200, 'Hit text route'));
+  }
+  else{
+    console.log("light is still on ");
+    // next(boom.create(500, 'Error reaching text'));
+  }
 });
 
-route.post('/logout', (req, res, next) => {
+router.post('/logout', (req, res, next) => {
   req.session = null;
   res.send();
 });
 
-module.exports = route;
+module.exports = router;
