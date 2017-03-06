ea890a0481246990aa79f8ed76afa8f344bde803
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-03-06 15:53:44 -0700

    fixed scope and route issues.   hopeful this time taht it will work

--- routes/data.js
+++ routes/data.js
@@ -29,35 +29,35 @@ router.post('/', (req, res, next) => {
       lightOnTime = req.body.smsLightOn;
       currentTime = req.body.currentTime;
   }
-  if(req.body.smsLightOn <= 0 && lightToggle <= 0){
+  if(req.body.light <= 0 && lightToggle <= 0){
     if(currentTime >= lightOnTime && currentTime <= lightOnTime){
-      //then it is ok for the light to be on and we do nothing
-      console.log("light is ON during scheduled ON time");
+      //light should be on during this time
+      lightToggle += 1;
+      var accountSid = 'AC674af2aaed607cbb23d6d2e718c30d6f';
+      var authToken = 'cceebb0dbcbfd2f072e45f83eae2b2b5';
+
+      //require the Twilio module and create a REST client
+      var client = require('twilio')(accountSid, authToken);
+
+      client.messages.create({
+        to: "+16109844474",//Me
+        // to: "+14848666955",//Keller
+
+        from: "+14846265179",
+        // body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+        body: "WARNING:  Lights have turned off out of schedule",
+
+      }, function(err, message) {
+        console.log(message.sid);
+      });
     }
-    else if (currentTime < lightOnTime && currentTime > lightOffTime){
-        console.log("light is OFF during scheduled ON time");
-    lightToggle += 1;
-    var accountSid = 'AC674af2aaed607cbb23d6d2e718c30d6f';
-    var authToken = 'cceebb0dbcbfd2f072e45f83eae2b2b5';
-
-    //require the Twilio module and create a REST client
-    var client = require('twilio')(accountSid, authToken);
-
-    client.messages.create({
-      to: "+16109844474",//Me
-      // to: "+14848666955",//Keller
-
-      from: "+14846265179",
-      // body: "This is the ship that made the Kessel Run in fourteen parsecs?",
-      body: "WARNING:  Lights have turned off out of scedule",
-
-    }, function(err, message) {
-      console.log(message.sid);
-    });
-  }
-  else if (req.body.smsLightOn >= 1 && lightToggle >= 1){
-    lightToggle -= 1;
-  }
+  //   else if (currentTime < lightOnTime && currentTime > lightOffTime){
+  //       console.log("light is OFF during scheduled ON time");
+  //
+  // }
+}
+else if (req.body.light >= 1 && lightToggle >= 1){
+  lightToggle -= 1;
 }
 
 if(req.body.humidity && req.body.temperature){
