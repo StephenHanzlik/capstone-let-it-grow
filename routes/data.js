08943ca35dc625f5945c553a8f8649a2ec26cbf8
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-03-07 13:32:47 -0700

    reverted post route to origonal state

--- routes/data.js
+++ routes/data.js
@@ -24,58 +24,52 @@ let lightOffTime = 0;
 let currentTime = 0;
 
 router.post('/', (req, res, next) => {
-  if(req.body.smsLightOn){
-    console.log("req.body.smsLightOn:");
-    console.log(req.body.smsLightOn);
-      lightOffTime = req.body.smsLightOff;
-      lightOnTime = req.body.smsLightOn;
-      currentTime = req.body.currentTime;
-      res.send(req.body);
-  }
-  if(req.body.light <= 0 && lightToggle <= 0){
-    console.log("req.body.light:");
-    console.log(req.body.light);
-    if(currentTime >= lightOnTime && currentTime <= lightOnTime){
-      console.log("currentTime:");
-      console.log(currentTime);
-      console.log(" VS lightOnTime:");
-      console.log(lightOnTime);
-      //light should be on during this time
-      lightToggle += 1;
-      var accountSid = 'AC674af2aaed607cbb23d6d2e718c30d6f';
-      var authToken = 'cceebb0dbcbfd2f072e45f83eae2b2b5';
+//   if(req.body.smsLightOn){
+//     console.log("req.body.smsLightOn:");
+//     console.log(req.body.smsLightOn);
+//       lightOffTime = req.body.smsLightOff;
+//       lightOnTime = req.body.smsLightOn;
+//       currentTime = req.body.currentTime;
+//       res.send(req.body);
+//   }
+//   if(req.body.light <= 0 && lightToggle <= 0){
+//     console.log("req.body.light:");
+//     console.log(req.body.light);
+//     if(currentTime >= lightOnTime && currentTime <= lightOnTime){
+//       console.log("currentTime:");
+//       console.log(currentTime);
+//       console.log(" VS lightOnTime:");
+//       console.log(lightOnTime);
+//       //light should be on during this time
+//       lightToggle += 1;
+//       var accountSid = 'AC674af2aaed607cbb23d6d2e718c30d6f';
+//       var authToken = 'cceebb0dbcbfd2f072e45f83eae2b2b5';
+//
+//       //require the Twilio module and create a REST client
+//       var client = require('twilio')(accountSid, authToken);
+//
+//       client.messages.create({
+//         to: "+16109844474",//Me
+//         // to: "+14848666955",//Keller
+//
+//         from: "+14846265179",
+//         // body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+//         body: "WARNING:  Lights have turned off out of schedule",
+//
+//       }, function(err, message) {
+//         console.log(message.sid);
+//         res.send(req.body);
+//       });
+//     }
+// }
+// else if (req.body.light >= 1 && lightToggle >= 1){
+//   console.log("light toggle");
+//   console.log(lightToggle);
+//   lightToggle -= 1;
+// }
 
-      //require the Twilio module and create a REST client
-      var client = require('twilio')(accountSid, authToken);
+// if(req.body.humidity && req.body.temperature){
 
-      client.messages.create({
-        to: "+16109844474",//Me
-        // to: "+14848666955",//Keller
-
-        from: "+14846265179",
-        // body: "This is the ship that made the Kessel Run in fourteen parsecs?",
-        body: "WARNING:  Lights have turned off out of schedule",
-
-      }, function(err, message) {
-        console.log(message.sid);
-        res.send(req.body);
-      });
-    }
-  //   else if (currentTime < lightOnTime && currentTime > lightOffTime){
-  //       console.log("light is OFF during scheduled ON time");
-  //
-  // }
-}
-else if (req.body.light >= 1 && lightToggle >= 1){
-  console.log("light toggle");
-  console.log(lightToggle);
-  lightToggle -= 1;
-}
-
-if(req.body.humidity && req.body.temperature){
-  console.log("ruglar data post route");
-  console.log("req.body.light: " + req.body.light);
-  console.log("lightToggle: " + lightToggle);
   const { light, temperature, humidity, soil_moisture } = req.body;
   const insertPost = { light, temperature, humidity, soil_moisture  };
   if(insertPost.humidity > 100){
@@ -99,7 +93,7 @@ if(req.body.humidity && req.body.temperature){
     .catch((err) => {
       next(err);
     });
-  }
+  // }//end of huimidt & control flow
 });
 
 
