cb02a6b696965f9a6e3034b5300baad16471992b
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-03-07 13:01:25 -0700

    commit before checkout

--- public/js/app/smsconfigure.component.js
+++ public/js/app/smsconfigure.component.js
@@ -18,14 +18,13 @@
       function onInit(){
           let promise = formService.getData();
           promise.then(function(response){
-            console.log("promise");
-            console.log(response);
             vm.data = response;
-            console.log(vm.data);
+
+
+
           })
       }
-      console.log("outside of function scope");
-      console.log(vm.data);
+
 
       function smsSettings(){
         let arr = [];
@@ -59,8 +58,7 @@
           }
           console.log(postObj);
 
-          // $http.post("https://limitless-river-10033.herokuapp.com/data", postObj)
-          $http.post("https://localhost:3000/data", postObj)
+          $http.post("https://limitless-river-10033.herokuapp.com/settings", postObj)
           .then(response => {
               console.log(response);
           });
@@ -71,67 +69,3 @@
     }
 
 }());
-
-
-
-// (function() {
-//   'use strict'
-//
-//   angular.module('app')
-//     .component('signUp', {
-//       controller: function($state, $http) {
-//         const vm = this;
-//         vm.signUp = signUp;
-//
-//         function signUp() {
-//           console.log(vm.signupForm);
-//           if (!vm.signupForm.Email) {
-//             console.log("Invalid Email");
-//           } else if (!vm.signupForm.Username) {
-//             console.log("Invalid Username");
-//           } else if (!vm.signupForm.Password) {
-//             console.log("Invalid Password");
-//           } else {
-//
-//             $http.post("http://localhost:3000/users/", vm.signupForm)
-//               .then(function(response) {
-//                 $state.go('mainLanding');
-//               })
-//
-//
-//             // vm.password = vm.signupForm.Password;
-//             // userService.makeNew(vm.signupForm.email, vm.signupForm.username, vm.signupForm.password)
-//             //   .then(function success(userData) {
-//             //     vm.data = userData;
-//             //     vm.data.password = vm.password;
-//             //     userService.login(vm.data.email, vm.data.password)
-//             //       .then((res) => {
-//             //         vm.data = res;
-//             //         $state.go('tabsController.profile');
-//             //       }, (error) => {
-//             //         $state.go('tabsController.login');
-//             //       });
-//             //   }, function error(error) {
-//             //     window.plugins.toast.showWithOptions({
-//             //       message: `Failed to create user: ${error.data}.\nPlease try again.`,
-//             //       duration: "long",
-//             //       position: "center",
-//             //       addPixelsY: -40
-//             //     });
-//             //   });
-//           }
-//         }
-//         // vm.$onInit = function() {
-//         //   vm.houses = houseService.houses
-//         // }
-//
-//         // vm.addHouse = function() {
-//         //   houseService.addHouse(vm.house)
-//         //   $state.go('show-house', { id: vm.house.id })
-//         // }
-//       },
-//       // template: `<h1>I am signup template</h1>`
-//       templateUrl: "js/app/signup.html"
-//     })
-//
-// }());
