5cb373074572b153e97a9ed15baa242fb41cd08b
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-03-07 16:30:46 -0700

    Form should be set up to post settings to PSQL

--- public/js/app/smsconfigure.component.js
+++ public/js/app/smsconfigure.component.js
@@ -27,10 +27,10 @@
         let arr = [];
         let arr2 = [];
         let nowArr = [];
-        let now = new Date();
+        // let now = new Date();
         let newString = vm.smsSettingForm.lightsOnInput.toString();
         let newString2 = vm.smsSettingForm.lightsOffInput.toString();
-        let nowString = now.toString();
+        // let nowString = now.toString();
           for(let i = 16; i <= 20; i++){
             if(newString.charAt(i) !== ":"){
               arr.push(newString.charAt(i));
@@ -38,24 +38,24 @@
             if(newString2.charAt(i) !== ":"){
               arr2.push(newString2.charAt(i));
             }
-            if(nowString.charAt(i) !== ":"){
-              nowArr.push(nowString.charAt(i));
-            }
+            // if(nowString.charAt(i) !== ":"){
+            //   nowArr.push(nowString.charAt(i));
+            // }
           }
           let joinedString = arr.join('');
           let joinedString2 = arr2.join('');
-          let joinedNowString = nowArr.join('');
-          let currentTime = parseInt(joinedNowString);
+          // let joinedNowString = nowArr.join('');
+          // let currentTime = parseInt(joinedNowString);
           let onTimeInt = parseInt(joinedString, 10);
           let onTimeInt2 = parseInt(joinedString2, 10);
           let postObj = {
             smsLightOn: onTimeInt,
-            smsLightOff: onTimeInt2,
-            currentTime: currentTime
+            smsLightOff: onTimeInt2
+            // currentTime: currentTime
           }
           console.log(postObj);
 
-          $http.post("https://limitless-river-10033.herokuapp.com/settings", postObj)
+          $http.post("https://limitless-river-10033.herokuapp.com/smssettings", postObj)
           .then(response => {
               console.log(response);
           });
