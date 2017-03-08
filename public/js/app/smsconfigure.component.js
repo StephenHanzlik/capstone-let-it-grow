9b17087f2858b6bc349d2d33dbb854666962b071
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-03-08 01:35:47 -0700

    missing a route

--- public/js/app/smsconfigure.component.js
+++ public/js/app/smsconfigure.component.js
@@ -60,7 +60,7 @@
           }
           // console.log(postObj);
 
-          $http.post("https://limitless-river-10033.herokuapp.com/smssettings", postObj)
+          $http.post("https://http://dinky-dinky.herokuapp.com/smssettings", postObj)
           .then(response => {
               console.log(response);
           });
