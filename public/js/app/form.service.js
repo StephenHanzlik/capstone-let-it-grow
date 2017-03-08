608cad53056d086e36d2006a94375258f8cf4b24
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-03-08 11:10:12 -0700

    pushing to heroki

--- public/js/app/form.service.js
+++ public/js/app/form.service.js
@@ -7,18 +7,18 @@
   service.$inject = ['$http'];
 
   function service($http) {
-  //   this.getData = function(){
-  //     return $http.get("http://dinkydinky.herokuapp.com/data").then(response => {
-  //      const data = response.data[0];
-  //      console.log(response.data[0]);
-  //      return data;
-  //    });
-  //  };
+    this.getData = function(){
+      return $http.get("http://dinkydinky.herokuapp.com/data").then(response => {
+       const data = response.data[0];
+       console.log(response.data[0]);
+       return data;
+     });
+   };
   //  this.dinky = this.getData();
-
-    this.sayHi = function (){
-      return "hello service";
-    };
+   //
+  //   this.sayHi = function (){
+  //     return "hello service";
+  //   };
 
   }
 
