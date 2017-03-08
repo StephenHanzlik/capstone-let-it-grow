30782395b456b73819e9a016aca53c46e76cb86f
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-03-07 20:24:45 -0700

    im and idiot there was a second text form...doooh

--- routes/data.js
+++ routes/data.js
@@ -232,24 +232,24 @@ console.log("made it to line 162");
 //       console.log(lightOnTime);
 //       //light should be on during this time
 //       lightToggle += 1;
-      var accountSid = 'AC674af2aaed607cbb23d6d2e718c30d6f';
-      var authToken = 'cceebb0dbcbfd2f072e45f83eae2b2b5';
-
-      //require the Twilio module and create a REST client
-      var client = require('twilio')(accountSid, authToken);
-
-      client.messages.create({
-        to: "+16109844474",//Me
-        // to: "+14848666955",//Keller
-
-        from: "+14846265179",
-        // body: "This is the ship that made the Kessel Run in fourteen parsecs?",
-        body: "WARNING:  Lights have turned off out of schedule",
-
-      }, function(err, message) {
-        console.log(message.sid);
-        res.send(req.body);
-      });
+      // var accountSid = 'AC674af2aaed607cbb23d6d2e718c30d6f';
+      // var authToken = 'cceebb0dbcbfd2f072e45f83eae2b2b5';
+      //
+      // //require the Twilio module and create a REST client
+      // var client = require('twilio')(accountSid, authToken);
+      //
+      // client.messages.create({
+      //   to: "+16109844474",//Me
+      //   // to: "+14848666955",//Keller
+      //
+      //   from: "+14846265179",
+      //   // body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+      //   body: "WARNING:  Lights have turned off out of schedule",
+      //
+      // }, function(err, message) {
+      //   console.log(message.sid);
+      //   res.send(req.body);
+      // });
 //     }
 // }
 // else if (req.body.light >= 1 && lightToggle >= 1){
