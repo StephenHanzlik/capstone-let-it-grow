6332353ea80b00710ffbc534175a3c8672adacc2
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-03-10 19:20:57 -0700

    logging in route

--- routes/data.js
+++ routes/data.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res, next) {
     .orderBy('id', 'desc')
     .limit(9)
     .then((result) => {
+      console.log(result);
       knex('data')
         .where('id', 10)
         .del()
